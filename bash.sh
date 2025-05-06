f75fcee1a01ee5d87659fd34fb6ae89b6ef7efba
u <u>
2025-05-06 20:40:28 +0300

    update

--- bash.sh
+++ bash.sh
@@ -2,7 +2,7 @@ name="aqute-bnd"
 cd "${outdir:-.}"
 git clone https://src.fedoraproject.org/rpms/"$name".git
 pushd "$name"
-sed -i 's/%bcond_with\s+bootstrap/%bcond_without\sbootstrap/' "$name".spec
+sed -i 's/bcond_with bootstrap/bcond_without bootstrap/g;' "$name".spec
 bash -x generate-tarball.sh
 popd
 mv "$name"/* ./
